b1c3432faefd2f1bfad0797d96a4dcc268e1f412
Himanshu Bisht <him.kec@gmail.com>
2023-07-26 13:52:25 -0700

    PMD example - adding new class to test pmd - 2

--- api/src/main/java/com/learn/PMDError.java
+++ api/src/main/java/com/learn/PMDError.java
@@ -13,20 +13,7 @@ public class PMDError {
     // Rule: ShortVariable
     private String x; // Violation: Variable name is too short
 
-    // Rule: UselessParentheses
-    public boolean isValid() {
-        return (id > 0); // Violation: Useless parentheses
-    }
-
-//    // Rule: DataflowAnomalyAnalysis
-//    public int divide(int a, int b) {
-//        int result;
-//        if (b != 0) {
-//            result = a / b;
-//        }
-//        return result; // Violation: The variable 'result' might not have been initialized
-//    }
-
+    
     // Rule: EmptyCatchBlock
     public void doSomething() {
         try {
